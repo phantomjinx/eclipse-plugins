98a2501b870fd8f40cc6551814d68d274dfc6c82
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2012-04-16 23:44:24 +0100

    Update license information for synchronizer plugin

--- plugins/org.phantomjinx.project.synchronizer/src/org/teiid/project/refresher/RefreshProjectHandler.java
+++ plugins/org.phantomjinx.project.synchronizer/src/org/teiid/project/refresher/RefreshProjectHandler.java
@@ -1,4 +1,22 @@
+/*
+ |  Copyright (C) 2012 Paul Richardson <phantom_sf at users sourceforge net>
+ |
+ |  This program is free software; you can redistribute it and/or modify
+ |  it under the terms of the GNU General Public License as published by
+ |  the Free Software Foundation; either version 2 of the License, or
+ |  (at your option) any later version.
+ |
+ |  This program is distributed in the hope that it will be useful,
+ |  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ |  GNU General Public License for more details.
+ |
+ |  You should have received a copy of the GNU General Public License
+ |  along with this program; if not, write to the Free Software
+ |  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package org.teiid.project.refresher;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -33,7 +51,6 @@ import org.eclipse.ui.IWorkingSetManager;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
-@SuppressWarnings("restriction")
 public class RefreshProjectHandler implements IHandler {
 
     private static final String DOT_PROJECT_FILE = ".project";
